236b471cb831550ba390cafd4b378ec5719e99f8
Giancarlo Di Massa <giancarlo@digitall.it>
2021-09-21 11:31:20 +0200

    Add namespace and config, IDE settings

---
+++ src/Extensions/MauticSiteConfig.php
@@ -0,0 +1,18 @@
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\ORM\DataExtension;
+
+class MauticSiteConfig extends DataExtension
+{
+
+private static $db = [
+'username' => 'Text'
+];
+
+public function updateCMSFields(FieldList $fields)
+{
+$fields->addFieldToTab("Root.Main",
+new HTMLEditorField("username", "User Name")
+);
+}
+}
