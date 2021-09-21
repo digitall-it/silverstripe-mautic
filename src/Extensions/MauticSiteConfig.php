f0638b1f14cfccaf478f95501125a85020e1e242
Giancarlo Di Massa <giancarlo@digitall.it>
2021-09-21 11:36:09 +0200

    Fix typo in SiteConfig

--- src/Extensions/MauticSiteConfig.php
+++ src/Extensions/MauticSiteConfig.php
@@ -1,20 +1,21 @@
+<?php
+
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\ORM\DataExtension;
 
 
-
 class SiteConfig extends DataExtension
 {
 
-private static $db = [
-'username' => 'Text'
-];
+    private static $db = [
+        'username' => 'Text'
+    ];
 
-public function updateCMSFields(FieldList $fields)
-{
-$fields->addFieldToTab("Root.Main",
-new HTMLEditorField("username", "User Name")
-);
-}
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab("Root.Main",
+            new HTMLEditorField("username", "User Name")
+        );
+    }
 }
