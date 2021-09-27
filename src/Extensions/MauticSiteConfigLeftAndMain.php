348a986074079eb9e89d7a625b125d43504917f2
Giancarlo Di Massa <giancarlo@digitall.it>
2021-09-27 11:05:36 +0200

    Add success message on flush

--- src/Extensions/MauticSiteConfigLeftAndMain.php
+++ src/Extensions/MauticSiteConfigLeftAndMain.php
@@ -5,11 +5,16 @@ namespace Digitall\Mautic\Extensions;
 use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Core\Extension;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Control\Controller;
 
 class MauticSiteConfigLeftAndMain extends Extension
 {
     public function doFlushMauticCache($data, $form)
     {
         Injector::inst()->get('Mautic')->flush();
+        Controller::curr()->getResponse()->setStatusCode(
+            200,
+            'Mautic cache flushed'
+        );
     }
 }
